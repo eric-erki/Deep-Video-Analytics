05555b9cba3aff28f59361423d8f3c726f6dddf7
akshay bhat <akshayubhat@gmail.com>
2018-01-20 00:20:42 -0800

    - Implement test script for creating training set and training LOPQ model
    Add train_lopq to perform training.
    
    Next:
    
    - Implement location net and clustering using PQKMeans
    
    - Implement training detection model using YOLO
    
    - Implement fine-tuning / training face recognition models

---
+++ server/dvaapp/operations/training.py
@@ -0,0 +1,22 @@
+import json, shutil, os
+import numpy as np
+from dvalib.trainers import lopq_trainer
+from dvaapp.models import TrainedModel, Retriever
+
+
+def train_lopq(training_set,vectors_path,args,output_dirname):
+    l = lopq_trainer.LOPQTrainer(name=args['source_indexer_shasum'],
+                                 dirname=output_dirname,
+                                 components=args['components'],m=args['m'],v=args['v'],sub=args['sub'],
+                                 source_indexer_shasum=args['source_indexer_shasum'])
+    data = np.load(vectors_path)
+    l.train(data)
+    j = l.save()
+    m = TrainedModel(**j)
+    m.training_set = training_set
+    m.save()
+    m.create_directory()
+    for f in m.files:
+        shutil.copy(f['url'],'{}/models/{}/{}'.format(settings.MEDIA_ROOT,m.pk,f['filename']))
+    dr = Retriever.objects.create(name="lopq retriever",source_filters={},
+                                  algorithm=Retriever.LOPQ, approximator_shasum=m.shasum)
