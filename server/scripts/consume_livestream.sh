67c527f165a735d5a19b59ae9ec436b52571a24c
akshay bhat <akshayubhat@gmail.com>
2018-02-03 13:08:10 -0800

    Improve reduce example.
    Next
    
    - Add validation before process is launched & enable garbage collection.
    
    Eventually:
    
    - Export models as a zip file.
    
    - Implement training detection model using YOLO
    
    - Implement location net and clustering using PQKMeans
    
    - Implement fine-tuning / training face recognition models
    
    - Pre-condition checks, and failed to launch error messages for processes.

---
+++ server/scripts/consume_livestream.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+livestreamer --player-continuous-http --twitch-oauth-token $TWITCH_TOKEN --player-no-close "$1" best -O --yes-run-as-root | ffmpeg -re -i - -c:v libx264 -c:a aac -ac 1 -strict -2 -crf 18 -profile:v baseline -maxrate 3000k -bufsize 1835k -pix_fmt yuv420p -flags -global_header -f segment -segment_time 0.1 %d.mp4
