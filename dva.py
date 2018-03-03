75bab3b9be950f57c6033e0ad7a8cb83946d2176
akshay bhat <akshayubhat@gmail.com>
2018-03-03 03:37:26 -0800

    Add a perform_test tasks to perform testing on map-reduce pipelines. Update Dockerfile with 1.6.0 from 1.6.0-rc1 Start working on adding FAISS based auto-tuned approximator, retriever and clustering. Add single dva.py to start containers on single node deployment rather than having multiple start, stop files.
    Eventually:
    
    - Pre-condition checks, and failed to launch error messages for processes.
    
    - Export models as a zip file.
    
    - Implement training detection model using YOLO
    
    - Implement location net, fine-tuning / training face recognition models

--- dva.py
+++ dva.py
@@ -7,55 +7,20 @@ import os
 import webbrowser
 
 
-def start_cpu_dev(subdir):
+def start(deployment_type, gpu_count):
     print "Checking if docker-compose is available"
     max_minutes = 20
-    try:
-        subprocess.check_call(["docker-compose", 'ps'],
-                              cwd=os.path.join(os.path.dirname(__file__), 'deploy/{}'.format(subdir)))
-    except:
-        raise SystemError("Docker-compose is not available")
-    print "Pulling/Refreshing container images, first time it might take a while to download the image"
-    try:
-        subprocess.check_call(["docker", 'pull', 'akshayubhat/dva-auto:latest'])
-    except:
-        raise SystemError("Docker is not running / could not pull akshayubhat/dva-auto:latest image from docker hub")
-    print "Trying to launch containers"
-    try:
-        compose_process = subprocess.Popen(["docker-compose", 'up', '-d'],
-                                           cwd=os.path.join(os.path.dirname(__file__), 'deploy/{}'.format(subdir)))
-    except:
-        raise SystemError("Could not start container")
-    while max_minutes:
-        print "Checking if DVA server is running, waiting for another minute and at most {max_minutes} minutes".format(
-            max_minutes=max_minutes)
-        try:
-            r = urllib2.urlopen("http://localhost:8000")
-            if r.getcode() == 200:
-                print "Open browser window and go to http://localhost:8000 to access DVA Web UI"
-                print 'Use following auth code to use jupyter notebook on  '
-                print subprocess.check_output(["docker", "exec", "-it", "webserver", "jupyter", 'notebook', 'list'])
-                print 'For windows you might need to replace "localhost" with ip address of docker-machine'
-                webbrowser.open("http://localhost:8000")
-                webbrowser.open("http://localhost:8888")
-                break
-        except:
-            pass
-        time.sleep(60)
-        max_minutes -= 1
-    compose_process.wait()
-
-
-def start_gpu(gpu_count):
-    print "Checking if docker-compose is available"
-    max_minutes = 20
-    if gpu_count == 1:
-        fname = 'docker-compose-gpu.yml'
+    if deployment_type == 'gpu':
+        if gpu_count == 1:
+            fname = 'docker-compose-gpu.yml'
+        else:
+            fname = 'docker-compose-{}-gpus.yml'.format(gpu_count)
     else:
-        fname = 'docker-compose-{}-gpus.yml'.format(gpu_count)
+        fname = 'docker-compose.yml'
+    print "Starting deploy/{}/{}".format(deployment_type, fname)
     try:
         subprocess.check_call(["docker-compose", 'ps'],
-                              cwd=os.path.join(os.path.dirname(__file__), 'deploy/gpu'))
+                              cwd=os.path.join(os.path.dirname(__file__), 'deploy/{}'.format(deployment_type)))
     except:
         raise SystemError("Docker-compose is not available")
     print "Pulling/Refreshing container images, first time it might take a while to download the image"
@@ -65,8 +30,9 @@ def start_gpu(gpu_count):
         raise SystemError("Docker is not running / could not pull akshayubhat/dva-auto:latest image from docker hub")
     print "Trying to launch containers"
     try:
-        compose_process = subprocess.Popen(["docker-compose", 'up', '-d', fname, ],
-                                           cwd=os.path.join(os.path.dirname(__file__), 'deploy/gpu'))
+        args = ["docker-compose",'-f',fname,'up','-d']
+        print " ".join(args)
+        compose_process = subprocess.Popen(args, cwd=os.path.join(os.path.dirname(__file__), 'deploy/{}'.format(deployment_type)))
     except:
         raise SystemError("Could not start container")
     while max_minutes:
@@ -87,6 +53,22 @@ def start_gpu(gpu_count):
         time.sleep(60)
         max_minutes -= 1
     compose_process.wait()
+
+
+def stop(deployment_type, gpu_count):
+    if deployment_type == 'gpu':
+        if gpu_count == 1:
+            fname = 'docker-compose-gpu.yml'
+        else:
+            fname = 'docker-compose-{}-gpus.yml'.format(gpu_count)
+    else:
+        fname = 'docker-compose.yml'
+    print "Stopping deploy/{}/{}".format(deployment_type, fname)
+    try:
+        subprocess.check_call(["docker-compose", '-f', fname, 'down'],
+                              cwd=os.path.join(os.path.dirname(__file__), 'deploy/{}'.format(deployment_type)))
+    except:
+        raise SystemError("Could not stop containers")
 
 
 if __name__ == '__main__':
@@ -97,9 +79,6 @@ if __name__ == '__main__':
                         type=int)
     args = parser.parse_args()
     if args.action == 'stop' or args.action == 'restart':
-        pass
+        stop(args.type, args.gpus)
     if args.action == 'start' or args.action == 'restart':
-        if args.type != 'gpu':
-            start_cpu_dev(args.type)
-        else:
-            start_gpu(args.gpus)
+        start(args.type, args.gpus)
