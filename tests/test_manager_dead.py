1008d03b5a8f16cbd9c781f4d7bcc54a6cdc5dd0
akshay bhat <akshayubhat@gmail.com>
2018-05-20 07:47:44 -0700

    Add last ping to worker which is updated by the manger to ensure. That workers without response from > 10 mins and pending tasks are killed and marked as dead. Add worker transition stats as well as last ping information to management page. Add migration. Add test for testing restart when worker + manager dies.
    Next:
    
    - Implement pre-condition checks, failed to launch error
    messages for processes. Rollback created models on failure to launch.
    
    - Implement clustering.
    
    - Implement location net, fine-tuning / training face recognition models

--- tests/test_manager_dead.py
+++ tests/test_manager_dead.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     subprocess.check_call(["wget","https://raw.githubusercontent.com/VisualDataNetwork/root/master/map_reduce/video.json"])
     subprocess.check_call(["./dvactl","exec","-f","video.json"])
     time.sleep(120)
-    os.system('docker exec -u="root" -it webserver ./../tests/kill_manager.sh')
+    os.system('docker exec -u="root" -it inception ./../tests/kill_manager.sh')
