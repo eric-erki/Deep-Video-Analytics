0cda46f3546938b5a68b3060b7c81c134218214b
akshay bhat <akshayubhat@gmail.com>
2018-06-01 00:48:41 -0700

    Add region relation to analysis. Implement K-NN graph constructions  using FAISS.
    Next:
    
    - Implement pre-condition checks, failed to launch error
    messages for processes. Rollback created models on failure to launch.
    
    - Implement clustering.
    
    - Implement location net, fine-tuning / training face recognition models

--- server/dvaapp/task_handlers.py
+++ server/dvaapp/task_handlers.py
@@ -224,5 +224,5 @@ def handle_perform_analysis(start):
         if target == 'regions':
             for i,k in enumerate(region_list):
                 relations.append(models.RegionRelation(source_region_id=source_regions[i].id,target_region_id=k.id,
-                                                       name='analysis_{}'.format(analyzer.name), event_id=start.pk))
+                                                       name='analysis', event_id=start.pk))
             models.RegionRelation.objects.bulk_create(relations, 1000)
