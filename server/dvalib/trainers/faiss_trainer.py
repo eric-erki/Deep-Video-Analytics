61cb1860cbf19b69589662aaa5f4a9b507bd6b33
akshay bhat <akshayubhat@gmail.com>
2018-06-02 03:50:32 -0700

    FAISS flat retriever works and tested on dev. Training faiss approximate index via index factory.
    - FAISS K-NN graph construction.
    
    Next:
    
    - Implement pre-condition checks, failed to launch error
    messages for processes. Rollback created models on failure to launch.
    
    - Implement clustering.
    
    - Implement location net, fine-tuning / training face recognition models

--- server/dvalib/trainers/faiss_trainer.py
+++ server/dvalib/trainers/faiss_trainer.py
@@ -8,7 +8,7 @@ except ImportError:
 
 
 def train_index(data, index_factory, output_file):
-    faiss_index = faiss.index_factory(data.shape[0], index_factory)
+    faiss_index = faiss.index_factory(data.shape[1], index_factory)
     faiss_index.add(data)
     faiss_index.train()
     faiss.write_index(faiss_index, output_file)
