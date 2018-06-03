f1bc421c1415d7f495765d935763f6d32a9b964d
akshay bhat <akshayubhat@gmail.com>
2018-06-03 01:51:59 -0700

    FAISS approximate indexer works!
    FAISS K-NN graph construction.
    
    Next:
    
    - Implement pre-condition checks, failed to launch error
    messages for processes. Rollback created models on failure to launch.
    
    - Implement clustering.
    
    - Implement location net, fine-tuning / training face recognition models

--- server/dvaapp/processing.py
+++ server/dvaapp/processing.py
@@ -420,10 +420,9 @@ class DVAPQLProcess(object):
         for t in self.process.script.get('reduce', []):
             if 'operation' not in t:
                 t['operation'] = 'perform_reduce'
-            if t['operation'] != 'perform_reduce':
                 self.launch_task(t)
             else:
-                raise ValueError('{} is not a valid reduce operation, only "perform_reduce" is valid'.format(
+                raise ValueError('{} is not a valid reduce operation, reduce tasks should not have an operation'.format(
                     t['operation']))
 
     def launch_query_tasks(self):
