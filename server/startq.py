a84cc4938c38904f6f4590b026c7b0aa133257fa
akshay bhat <akshayubhat@gmail.com>
2018-06-04 00:16:41 -0700

    Log to stderr in kubemode. Add trainer worker to Kubernetes workers. Test Faiss using Kubernetes.
    Soon: Export trained approximators. FAISS K-NN graph.
    
    Next:
    
    - Implement pre-condition checks, failed to launch error
    messages for processes. Rollback created models on failure to launch.
    
    - Implement clustering.
    
    - Implement location net, fine-tuning / training face recognition models

--- server/startq.py
+++ server/startq.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import django, os, subprocess, sys, logging, shlex
+
 sys.path.append(os.path.dirname(__file__))
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -7,12 +8,21 @@ logging.basicConfig(level=logging.INFO,
                     filename='../logs/startq.log',
                     filemode='a')
 
+
+def log_output(queue_name, settings):
+    if settings.KUBE_MODE:
+        return ""
+    else:
+        return "-f ../logs/{}.log".format(queue_name)
+
+
 if __name__ == "__main__":
     # TODO: worker failing due to
     # https://github.com/celery/celery/issues/3620
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dva.settings")
     django.setup()
     from django.conf import settings
+
     queue_name = sys.argv[1]
     if len(sys.argv) > 2 and sys.argv[2] != '&':
         conc = int(sys.argv[2])
@@ -27,27 +37,27 @@ if __name__ == "__main__":
         except:
             logging.exception("Could not update youtube-dl")
             pass
-        command = 'celery -A dva worker -l info {} -c {} -Q {} -n {}.%h -f ../logs/{}.log'.format(mute, max(int(conc), 4),
-                                                                                                  queue_name, queue_name,
-                                                                                                  queue_name)
+        command = 'celery -A dva worker -l info {} -c {} -Q {} -n {}.%h {}'.format(mute, max(int(conc), 4),
+                                                                                   queue_name, queue_name,
+                                                                                   log_output(queue_name, settings))
     elif queue_name == settings.Q_STREAMER:
         try:
-            subprocess.check_output(['pip','install','--upgrade','streamlink','psutil'])
+            subprocess.check_output(['pip', 'install', '--upgrade', 'streamlink', 'psutil'])
         except:
             logging.exception("Could not install streamlink")
             pass
-        command = 'celery -A dva worker -l info {} -c {} -Q {} -n {}.%h -f ../logs/{}.log'.format(mute, max(int(conc), 2),
-                                                                                                  queue_name, queue_name,
-                                                                                                  queue_name)
+        command = 'celery -A dva worker -l info {} -c {} -Q {} -n {}.%h {}'.format(mute, max(int(conc), 2),
+                                                                                   queue_name, queue_name,
+                                                                                   log_output(queue_name, settings))
     elif queue_name == settings.Q_REDUCER:
-        command = 'celery -A dva worker -l info {} -c {} -Q {} -n {}.%h -f ../logs/{}.log'.format(mute, max(int(conc), 4),
-                                                                                                  queue_name, queue_name,
-                                                                                                  queue_name)
+        command = 'celery -A dva worker -l info {} -c {} -Q {} -n {}.%h {}'.format(mute, max(int(conc), 4),
+                                                                                   queue_name, queue_name,
+                                                                                   log_output(queue_name, settings))
     else:
-        command = 'celery -A dva worker -l info {} -P solo -c {} -Q {} -n {}.%h -f ../logs/{}.log'.format(mute, 1,
-                                                                                                          queue_name,
-                                                                                                          queue_name,
-                                                                                                          queue_name)
+        command = 'celery -A dva worker -l info {} -P solo -c {} -Q {} -n {}.%h {}'.format(mute, 1, queue_name,
+                                                                                           queue_name,
+                                                                                           log_output(queue_name,
+                                                                                                      settings))
     logging.info(command)
     c = subprocess.Popen(args=shlex.split(command))
     c.wait()
