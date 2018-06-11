1e4136f57bf16ddc8f801facb493812e265e9104
akshay bhat <akshayubhat@gmail.com>
2018-06-11 01:48:29 -0700

    YFCC processing and preparation scripts to VDN. Hyper Region relations and Hyper Tube Regions Relations for storing output of K-NN Matching and other relationships. Add test file perform_matching, ensure that events are in completed state before performing matching. Update models.py to ensure that model shasum is stored before. Exclude selected source video from targets when performing matching.
    Next:
    
    - Export trained approximators as zip file.
    
    - Implement pre-condition checks, failed to launch error
    messages for processes. Rollback created models on failure to launch.
    
    - Add location net, fine-tuning / training face recognition models

--- server/dvalib/retriever.py
+++ server/dvalib/retriever.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy import spatial
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 import uuid
 import sys
 
@@ -176,10 +176,10 @@ class FaissFlatRetriever(BaseRetriever):
         if vectors.shape[-1] != self.components:
             vectors = vectors.T
         dist, ids = self.faiss_index.search(vectors, n)
-        results = {}
-        for k in range(ids.shape[0]):
+        results = defaultdict(list)
+        for vindex in range(ids.shape[0]):
             for i, k in enumerate(ids[k]):
-                temp = {'rank': i + 1, 'algo': self.name, 'dist': float(dist[k, i])}
+                temp = {'rank': i + 1, 'algo': self.name, 'dist': float(dist[vindex, i])}
                 temp.update(self.files[k])
-                results[k].append(temp)
+                results[vindex].append(temp)
         return results
