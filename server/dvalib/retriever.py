07fa793d3acba010d2f43cb684465db9ab44335e
akshay bhat <akshayubhat@gmail.com>
2018-06-11 01:50:07 -0700

    YFCC processing and preparation scripts to VDN. Hyper Region relations and Hyper Tube Regions Relations for storing output of K-NN Matching and other relationships. Add test file perform_matching, ensure that events are in completed state before performing matching. Update models.py to ensure that model shasum is stored before. Exclude selected source video from targets when performing matching.
    Next:
    
    - Export trained approximators as zip file.
    
    - Implement pre-condition checks, failed to launch error
    messages for processes. Rollback created models on failure to launch.
    
    - Add location net, fine-tuning / training face recognition models

--- server/dvalib/retriever.py
+++ server/dvalib/retriever.py
@@ -5,6 +5,7 @@ import uuid
 import sys
 
 import logging
+
 try:
     from sklearn.decomposition import PCA
     from lopq import LOPQModel, LOPQSearcher
@@ -14,20 +15,18 @@ try:
 except ImportError:
     logging.warning("Could not import lopq")
 
-
 try:
     sys.path.append('/root/thirdparty/faiss/python')
     import faiss
 except ImportError:
     logging.warning("could not import FAISS")
 
-
-IndexRange = namedtuple('IndexRange',['start','end'])
+IndexRange = namedtuple('IndexRange', ['start', 'end'])
 
 
 class BaseRetriever(object):
 
-    def __init__(self,name,approximator=None,algorithm="EXACT"):
+    def __init__(self, name, approximator=None, algorithm="EXACT"):
         self.name = name
         self.algorithm = algorithm
         self.approximate = False
@@ -37,7 +36,7 @@ class BaseRetriever(object):
         self.index, self.files, self.findex = None, {}, 0
         self.support_batching = False
 
-    def load_index(self,numpy_matrix,entries):
+    def load_index(self, numpy_matrix, entries):
         temp_index = [numpy_matrix, ]
         for i, e in enumerate(entries):
             self.files[self.findex] = e
@@ -56,22 +55,23 @@ class BaseRetriever(object):
             vector = np.atleast_2d(self.approximator.approximate(vector))
         if self.index is not None:
             try:
-                dist = spatial.distance.cdist(vector,self.index)
+                dist = spatial.distance.cdist(vector, self.index)
             except:
-                raise ValueError("Could not compute distance Vector shape {} and index shape {}".format(vector.shape, self.index.shape))
+                raise ValueError("Could not compute distance Vector shape {} and index shape {}".format(vector.shape,
+                                                                                                        self.index.shape))
         if dist is not None:
             ranked = np.squeeze(dist.argsort())
             for i, k in enumerate(ranked[:n]):
-                temp = {'rank':i+1,'algo':self.name,'dist':float(dist[0,k])}
+                temp = {'rank': i + 1, 'algo': self.name, 'dist': float(dist[0, k])}
                 temp.update(self.files[k])
                 results.append(temp)
-        return results # Next also return computed query_vector
+        return results  # Next also return computed query_vector
 
 
 class LOPQRetriever(BaseRetriever):
 
-    def __init__(self,name,approximator):
-        super(LOPQRetriever, self).__init__(name=name,approximator=approximator,algorithm="LOPQ")
+    def __init__(self, name, approximator):
+        super(LOPQRetriever, self).__init__(name=name, approximator=approximator, algorithm="LOPQ")
         self.approximate = True
         self.name = name
         self.loaded_entries = {}
@@ -81,20 +81,20 @@ class LOPQRetriever(BaseRetriever):
         self.approximator.load()
         self.searcher = LOPQSearcher(model=self.approximator.model)
 
-    def load_index(self,numpy_matrix=None,entries=None):
+    def load_index(self, numpy_matrix=None, entries=None):
         codes = []
         ids = []
         last_index = len(self.entries)
         for i, e in enumerate(entries):
-            codes.append((tuple(e['codes'][0]),tuple(e['codes'][1])))
-            ids.append(i+last_index)
+            codes.append((tuple(e['codes'][0]), tuple(e['codes'][1])))
+            ids.append(i + last_index)
             self.entries.append(e)
-        self.searcher.add_codes(codes,ids)
+        self.searcher.add_codes(codes, ids)
 
-    def nearest(self,vector=None,n=12):
+    def nearest(self, vector=None, n=12):
         results = []
         pca_vec = self.approximator.get_pca_vector(vector)
-        results_indexes, visited = self.searcher.search(pca_vec,quota=n)
+        results_indexes, visited = self.searcher.search(pca_vec, quota=n)
         for r in results_indexes:
             results.append(self.entries[r.id])
         return results
@@ -102,15 +102,15 @@ class LOPQRetriever(BaseRetriever):
 
 class FaissApproximateRetriever(BaseRetriever):
 
-    def __init__(self,name, approximator):
+    def __init__(self, name, approximator):
         super(FaissApproximateRetriever, self).__init__(name=name, approximator=approximator, algorithm="FAISS")
-        self.index_path = str(approximator.index_path).replace('//','/')
+        self.index_path = str(approximator.index_path).replace('//', '/')
         self.ivfs = []
         self.ivf_vector = faiss.InvertedListsPtrVector()
-        self.uuid = str(uuid.uuid4()).replace('-','_')
+        self.uuid = str(uuid.uuid4()).replace('-', '_')
         self.faiss_index = None
 
-    def load_index(self,computed_index_path,entries):
+    def load_index(self, computed_index_path, entries):
         if len(entries):
             computed_index_path = str(computed_index_path).replace('//', '/')
             logging.info("Adding {}".format(computed_index_path))
@@ -121,12 +121,12 @@ class FaissApproximateRetriever(BaseRetriever):
                 self.faiss_index = faiss.read_index(computed_index_path)
             else:
                 index = faiss.read_index(computed_index_path)
-                self.faiss_index.merge_from(index,self.faiss_index.ntotal)
+                self.faiss_index.merge_from(index, self.faiss_index.ntotal)
             logging.info("Index size {}".format(self.faiss_index.ntotal))
 
     def nearest(self, vector=None, n=12, nprobe=16):
         logging.info("Index size {} with {} loaded entries in {}".format(self.faiss_index.ntotal,
-                                                                            len(self.loaded_entries),self.name))
+                                                                         len(self.loaded_entries), self.name))
         self.faiss_index.nprobe = nprobe
         vector = np.atleast_2d(vector)
         if vector.shape[-1] != self.faiss_index.d:
@@ -142,14 +142,14 @@ class FaissApproximateRetriever(BaseRetriever):
 
 class FaissFlatRetriever(BaseRetriever):
 
-    def __init__(self,name, components, metric='Flat'):
+    def __init__(self, name, components, metric='Flat'):
         super(FaissFlatRetriever, self).__init__(name=name, algorithm="FAISS_{}".format(metric))
-        self.name=name
+        self.name = name
         self.components = components
-        self.algorithm="FAISS_{}".format(metric)
+        self.algorithm = "FAISS_{}".format(metric)
         self.faiss_index = faiss.index_factory(components, metric)
 
-    def load_index(self,numpy_matrix,entries):
+    def load_index(self, numpy_matrix, entries):
         if len(entries):
             logging.info("Adding {}".format(numpy_matrix.shape))
             numpy_matrix = np.atleast_2d(numpy_matrix.squeeze())
@@ -178,7 +178,7 @@ class FaissFlatRetriever(BaseRetriever):
         dist, ids = self.faiss_index.search(vectors, n)
         results = defaultdict(list)
         for vindex in range(ids.shape[0]):
-            for i, k in enumerate(ids[k]):
+            for i, k in enumerate(ids[vindex]):
                 temp = {'rank': i + 1, 'algo': self.name, 'dist': float(dist[vindex, i])}
                 temp.update(self.files[k])
                 results[vindex].append(temp)
