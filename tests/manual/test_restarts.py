e03eeafc517f5c0aa5d3485f40507fdf0e3c423c
akshay bhat <akshayubhat@gmail.com>
2018-06-16 21:09:08 -0700

    Bump TF version in travis to 1.8 move kill.sh to scripts and remove un-necessary files.Re-organize tests.
    Soon: YFCC processing and preparation scripts to VDN.
    
    Next:
    
    - Export trained approximators as zip file.
    
    - Implement pre-condition checks, failed to launch error
    messages for processes. Rollback created models on failure to launch.
    
    - Add location net, fine-tuning / training face recognition models

--- tests/manual/test_restarts.py
+++ tests/manual/test_restarts.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     subprocess.check_call(["wget","https://raw.githubusercontent.com/VisualDataNetwork/root/master/map_reduce/video.json"])
     subprocess.check_call(["./dvactl","exec","-f","video.json"])
     time.sleep(120)
-    os.system('docker exec -u="root" -it inception ./../tests/kill_manager.sh')
+    os.system('docker exec -u="root" -it inception ./scripts/kill_manager.sh')
