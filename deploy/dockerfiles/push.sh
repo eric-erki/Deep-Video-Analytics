5371e636ff2b031545a8e8e5b2ef8ce0c3a5573d
akshay bhat <akshayubhat@gmail.com>
2018-08-11 19:04:01 -0700

    Cancel image upgrade temporarily.

--- deploy/dockerfiles/push.sh
+++ deploy/dockerfiles/push.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-set -xe
+set -x
 docker rmi akshayubhat/dva-auto:latest
 docker rmi akshayubhat/dva-auto:gpu
 docker build -t akshayubhat/dva-auto:latest .
 docker build -t akshayubhat/dva-auto:gpu -f Dockerfile.gpu .
-docker push akshayubhat/dva-auto:latest
-docker push akshayubhat/dva-auto:gpu
